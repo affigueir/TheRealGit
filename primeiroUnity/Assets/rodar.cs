31e78254eefe317d9b756365b0c0d98a9d54e836
André Furão Figueira <affigueir@gmail.com>
2020-02-20 12:15:54 +0000

    First Commit

---
+++ primeiroUnity/Assets/rodar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rodar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(9999999999, 999999999, 999999999));
+        transform.Translate(new Vector3(0.99f, 0, 0.99f));    
+    }
+}
